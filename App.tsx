d51ec34183684ad7a62ccc3fe102ad171b0b5934
George Madeley <prof.chewbacca@gmail.com>
2023-03-15 11:36:47 +0000

    Working with Emulator

--- App.tsx
+++ App.tsx
@@ -2,13 +2,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 
-import ConnectedScreen from "./src/screens/connected_screen/connected_screen";
 import HelpScreen from "./src/screens/help_screen/help_screen";
 import HomeScreen from "./src/screens/home_screen/home_screen";
-import ReadingCompleteScreen from "./src/screens/reading_complete_screen/reading_complete_screen";
-import ReadingInProgressScreen from "./src/screens/reading_in_progress_screen/reading_in_progress_screen";
-import ReadingScreen from "./src/screens/reading_screen/reading_screen";
-import ViewLastReadingScreen from "./src/screens/view_last_reading_screen/view_last_reading_screen";
+import AccountScreen from "./src/screens/account_screen/account_screen";
+import MapScreen from "./src/screens/map_screen/map_screen";
+import NewsScreen from "./src/screens/news_screen/news_screen";
+import ReadingScreen from "./src/screens/readings_screen/readings_screen";
+import ReportScreen from "./src/screens/report_screen/report_screen";
+import ViewNewsScreen from "./src/screens/view_news_screen/view_news_screen";
+import ViewReadingScreen from "./src/screens/view_readings_screen/view_readings_screen";
 
 
 const Stack : any = createNativeStackNavigator();
@@ -18,12 +20,14 @@ export default function App() : JSX.Element {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false, gestureEnabled: false }} >
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Connected" component={ConnectedScreen} />
-        <Stack.Screen name="ReadingInProgress" component={ReadingInProgressScreen} />
-        <Stack.Screen name="ReadingComplete" component={ReadingCompleteScreen} />
-        <Stack.Screen name="ViewLastReading" component={ViewLastReadingScreen} />
-        <Stack.Screen name="Readings" component={ReadingScreen} />
         <Stack.Screen name="Help" component={HelpScreen} />
+        <Stack.Screen name="Account" component={AccountScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
+        <Stack.Screen name="News" component={NewsScreen} />
+        <Stack.Screen name="Readings" component={ReadingScreen} />
+        <Stack.Screen name="Report" component={ReportScreen} />
+        <Stack.Screen name="ViewNews" component={ViewNewsScreen} />
+        <Stack.Screen name="ViewReadings" component={ViewReadingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
