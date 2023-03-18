36188aae00ed06142b121d26688055dffa1b9401
George Madeley <prof.chewbacca@gmail.com>
2023-03-18 19:16:59 +0000

    Adjusted View Readings Title Margin

--- src/screens/view_readings_screen/view_readings_styles.ts
+++ src/screens/view_readings_screen/view_readings_styles.ts
@@ -6,7 +6,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start',
-    paddingTop: '5%',
+    paddingTop: '10%',
   },
   body: {
     width: '100%',
