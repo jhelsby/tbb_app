69e8d9838389cc141138f076a06a0ed466b5d975
George Madeley <prof.chewbacca@gmail.com>
2023-05-05 13:04:54 +0100

    Preparing for new_design merge

--- src/screens/rootScreensPrams.ts
+++
@@ -1,9 +0,0 @@
-export type RootStackParamList = {
-  Home: undefined;
-  Connected: undefined;
-  Help: undefined;
-  ReadingComplete: undefined;
-  ReadingInProgress: undefined;
-  Reading: undefined;
-  ViewLastReading: undefined;
-};
